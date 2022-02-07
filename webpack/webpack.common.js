0336c55394a39e6a33c8c6efe53b7e4efd91f659
Mikael Escolin <mikaelescolin@gmail.com>
2022-02-07 18:33:55 +0000

    Add loader for scss
    - Installed packages to handle scss in webpack
    - Configured webpack for scss

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,43 +1,57 @@
-const webpack = require("webpack");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const srcDir = path.join(__dirname, "..", "src");
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = path.join(__dirname, '..', 'src');
 
 module.exports = {
-    entry: {
-      popup: path.join(srcDir, 'popup.tsx'),
-      options: path.join(srcDir, 'options.tsx'),
-      background: path.join(srcDir, 'background.ts'),
-      content_script: path.join(srcDir, 'content_script.tsx'),
+  entry: {
+    popup: path.join(srcDir, 'popup.tsx'),
+    options: path.join(srcDir, 'options.tsx'),
+    background: path.join(srcDir, 'background.ts'),
+    content_script: path.join(srcDir, 'content_script.tsx'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist/js'),
+    filename: '[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks(chunk) {
+        return chunk.name !== 'background';
+      },
     },
-    output: {
-        path: path.join(__dirname, "../dist/js"),
-        filename: "[name].js",
-    },
-    optimization: {
-        splitChunks: {
-            name: "vendor",
-            chunks(chunk) {
-              return chunk.name !== 'background';
-            }
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          {
+            loader: 'css-loader',
+            options: { modules: true },
+          },
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '.', to: '../', context: 'public' }],
+      options: {},
+    }),
+  ],
 };
